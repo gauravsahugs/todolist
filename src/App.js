9b8aab3efb84d0cc4656a0d900799579e4f79ae9
Gaurav Sahu <sahugaurav80@gmail.com>
2022-07-28 15:02:46 +0530

    updating

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 function App() {
   const [message, setMessage] = useState("");
+  const [dueDate, setDueDate] = useState("");
   const [todoList, setTodoList] = useState([]);
 
   const addTodo = function () {
@@ -16,25 +17,68 @@ function App() {
     setMessage(e.target.value);
   };
 
+  const handleChangeDate = (e) => {
+    e.preventDefault();
+    setDueDate(e.target.value);
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    e.target.style.setProperty("text-decoration", "line-through");
+    e.target.style.color = "#82E0AA";
+  };
+  const handleDoubleClick = (e) => {
+    e.preventDefault();
+    e.target.style.setProperty("text-decoration", "");
+    e.target.style.color = "";
+  };
+
+  const date = new Date();
+  const timestamp =
+    date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
+  console.log(timestamp);
   return (
     <div className="app">
       <div className="app_body">
         <div className="header">
           <h1>Todo-List </h1>
-          <input
-            className="input_message"
-            type="text"
-            onChange={handleChange}
-            placeholder="write here"
-            value={message}
-          />
-          <button className="add_todo" onClick={addTodo}>
-            Add Todo
-          </button>
+          <div className="inputfield">
+            <input
+              className="input_message"
+              type="text"
+              onChange={handleChange}
+              placeholder="Add your task here"
+              value={message}
+            />
+            <input
+              type="date"
+              onChange={handleChangeDate}
+              value={dueDate}
+              placeholder="MM-DD-YYYY"
+            />
+            Task Completion date
+            <button className="add_todo" onClick={addTodo}>
+              Add Todo
+            </button>
+          </div>
         </div>
         <div className="listitem">
           {todoList.map((todo) => {
-            return <li>{todo}</li>;
+            return (
+              <div>
+                <li
+                  className="todolistitem"
+                  onClick={handleClick}
+                  onDoubleClick={handleDoubleClick}
+                >
+                  {todo}
+                </li>
+                <div className="timestamp">
+                  <>Task pinned on: {timestamp} </>
+                  <>Finish task by:{dueDate}</>
+                </div>
+              </div>
+            );
           })}
         </div>
       </div>
